c71ee4aed2e4bf7b49c80f4debf72a3b3d31a567
Dawid <dawidfirmowski@gmail.com>
2023-08-17 12:40:17 +0200

    create login/register views

---
+++ account/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    # check if user is in database
+    def clean(self):
+        cleaned_data = super().clean()
+        user = authenticate(**cleaned_data)
+        if user is None:
+            raise ValidationError("There is no matching user in the system")
+        cleaned_data["user"] = user
+        return cleaned_data
+
+
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Type password again")
+    email = forms.CharField(widget=forms.EmailField, label="Email")
+    username = forms.CharField(initial="")
+
+    # check if both password are the same
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data["password1"] != cleaned_data["password2"]:
+            raise ValidationError("Passwords don't match")
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
--- account/views.py
+++ account/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login
+from django.views import View
 
-# Create your views here.
+from account.forms import LoginForm, RegisterForm
+
+
+class LoginView(View):
+
+    # render login page
+    def get(self, request):
+        form = LoginForm()
+        return render(request, "account/form.html", {"form": form})
+
+    # check user's data and log him in if corrects
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data["user"]
+            if user is not None:
+                login(request, user)
+            return redirect("")
+
+        return render(request, "account/form.html", {"form": form})
+
+
+class LogoutView(View):
+
+    # logout user
+    def get(self, request):
+        logout(request)
+        return redirect("")
+
+
+class RegisterView(View):
+
+    # render register page
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, "account/form.html", {"form": form})
+
+    # get the data from user's form and create user
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password1"])
+            user.save()
+            return redirect("")
+
+        return render(request, "account/form.html", {"form": form})
