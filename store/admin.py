6936efc0a706fedf127a7225ce0dfbc5bfcb26c8
Dawid <dawidfirmowski@gmail.com>
2023-09-06 11:47:15 +0200

    models adjust

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from store.models import *
 
-admin.site.register([Product, Cart, CartProduct, Category, Order, ProductImage])
+admin.site.register([Customer, Category, Product, ProductImage, Order, OrderItem, ShippingAddress])
+
